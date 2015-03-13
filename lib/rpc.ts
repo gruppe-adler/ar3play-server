19c088b811eb144bafaeec7694c9fca8a3f49d2c
Moritz Schmidt <fusselwurm@gmail.com>
2015-03-13 17:50:28 +0100

    debounce, end mission after 30s

--- lib/rpc.ts
+++ lib/rpc.ts
@@ -1,6 +1,6 @@
 /// <reference path="./../typings/tsd.d.ts" />
 
-import underscore = require('underscore');
+import _ = require('underscore');
 import persist = require('./persist');
 import PlayerInfo = require('./PlayerInfo');
 import bunyan = require('bunyan');
@@ -11,11 +11,19 @@ var
 
 logger.level('debug');
 
+
+var watchForEnd = _.debounce(function () {
+    logger.warn('timeout! declaring mission as ended');
+    persist.missionEnd();
+}, 30000);
+
+
 export function init(port) {
     registerAll();
 
     rpc.listen("::1", port);
     logger.info('listening for RPC calls on port ' + port);
+    watchForEnd();
 }
 
 function registerAll() {
@@ -37,11 +45,13 @@ function registerAll() {
      *  Get date (no arguments)
      */
     rpc.register('getDate', function (callback) {
+        watchForEnd();
         logger.debug('getDate called :)');
         callback(null, new Date().toString());
     });
 
     rpc.register('missionStart', function (missionName: string, worldname: string, callback: Function) {
+        watchForEnd();
         persist.missionStart(missionName, worldname);
         callback(null, 201);
     });
@@ -53,20 +63,20 @@ function registerAll() {
     });
 
     rpc.register('setIsStreamable', function (isStreamable: boolean, cb: Function) {
+        watchForEnd();
         persist.setIsStreamable(isStreamable);
         cb(null, 201);
     });
 
     rpc.register('setPlayerPosition', function (name, position, callback) {
-        console.log(name + ': ' + position.map(function (p) {
-            return p.toFixed(0);
-        }).join('/'));
+        watchForEnd();
 
         persist.setPlayerPosition(name, new PlayerInfo.Point(position[0].toFixed(0), position[1].toFixed(0)));
         callback(null, 201);
     });
 
     rpc.register('setPlayerSide', function (playerName: string, side: string, cb) {
+        watchForEnd();
         logger.debug('playerside ' + playerName + ': ' + side);
         persist.setPlayerSide(playerName, PlayerInfo.Side.fromGameSide(side));
 
@@ -74,9 +84,9 @@ function registerAll() {
     });
 
     rpc.register('setPlayerStatus', function (playerName: string, status: string, callback) {
+        watchForEnd();
         logger.debug('playerstatus ' + playerName + ': ' + status);
         persist.setPlayerStatus(playerName, status);
         callback(null, 201);
     });
-
 }
